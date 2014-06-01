5fa1b51e8cb9ad64b96d0f252219e1f538247282
Vlad Lastname <warangel.alk@gmail.com>
2014-06-01 10:18:36 +0300

    Added LogoutController

---
+++ src/main/java/com/my/controller/spring/LogoutController.java
@@ -0,0 +1,23 @@
+package com.my.controller.spring;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vlad
+ * Date: 18.05.14
+ * Time: 13:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class LogoutController extends AbstractController {
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+
+}
